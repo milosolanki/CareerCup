71fc3e6306e1ead8de2e7fb173c60a9bfc76fba8
milo <milosolanki@gmail.com>
2018-04-22 22:48:13 +0800

    2.2

---
+++ 2_2.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+	int data;
+	Node *next = NULL;
+	Node(int d){	this->data = d;	}
+	void appendToTail(int d){
+		Node *end = new Node(d);
+		Node *n = this;
+		while(n->next!=NULL)	n = n->next;
+		n->next = end;
+	}
+};
+void printList(Node* head){
+	Node *n = head;
+	while(n!=NULL){
+		cout << n->data << " ";
+		n = n->next;
+	} 
+	cout << endl;
+}
+int findNFromLast(Node *head, int n){
+	Node *p1=head,*p2=head;
+	while(n--)	p2 = p2->next;
+	while(p2->next!=NULL){
+		p1 = p1->next;
+		p2 = p2->next;
+	}
+	return p1->data;
+}
+int main(){
+	Node *head = new Node(1);
+	head->appendToTail(6);
+	head->appendToTail(2);
+	head->appendToTail(3);
+	head->appendToTail(4);
+	head->appendToTail(7);
+	head->appendToTail(5);
+	head->appendToTail(3);
+	head->appendToTail(6);
+	head->appendToTail(2);
+	head->appendToTail(7);
+	printList(head);
+	cout << findNFromLast(head,5) << endl;
+	return 0;
+}
